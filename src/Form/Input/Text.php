0a16653d008ef78763dab41646e7c71c77a5a47b
Виктор Гвоздиков <starosta-91@ya.ru>
2025-07-22 14:33:11 +0300

    Update Text.php

--- src/Form/Input/Text.php
+++ src/Form/Input/Text.php
@@ -8,12 +8,26 @@ use ModuleBZ\Form\Input;
 class Text extends Input {
     /** @var ?string Маска ввода  */
     var $mask = null;
+    /** @var array $options варианты выбора */
+    protected $options = [];
 
     protected function getType(): int { return EType::TEXT; }
 
+    /**
+     * Установка варантов выбора
+     * @param array $options список вариантов ['id'=>number,'title'=>string]
+     * @return Text
+     */
+    public function setOptions(array $options) {
+        $this->options = $options;
+        return $this;
+    }
+
+
     public function toJSON(): array {
         $res = parent::toJSON();
-        $res['input_opt']['mask'] = $this->mask;
+        $res['input_opt']['mask']    = $this->mask;
+        $res['input_opt']['options'] = $this->options;
         return $res;
     }
     /**
